014399b5b454d533ce73c26a35b0d8334b405d38
Artem <nimoshka@gmail.com>
2022-05-06 22:31:15 +0300

    Completed challenge
    Forgot earlyer commits :P
    Add button to reset grid with custom size

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-const quantity = 256
-document.onload = addDivs(quantity);
+let resolution = 16;
+document.onload = addDivs(resolution);
 
-function addDivs(q) {
-    for (let i = 1; i <= q; i++) {
+const button = document.querySelector('button');
+
+//Reset button
+button.addEventListener('click', () => {
+    document.querySelectorAll('.square').forEach((div) => {
+        document.querySelector('.container').removeChild(div);
+    });
+
+    enterNewResolution()
+});
+
+//Prompt validation
+function enterNewResolution(){
+    let enter = prompt('Enter square grid resolution (100 max):');
+
+    if(enter <= 100){
+        resolution = enter;
+        addDivs(resolution);
+    } else {
+        enterNewResolution();
+    }
+}
+
+//Grid constructor
+function addDivs(r) {
+
+    for (let i = 1; i <= r ** 2; i++) {
         const newDiv = document.createElement('div');
-        newDiv.classList.add('square', 'div_'+ i);
+        newDiv.classList.add('square', 'div_' + i);
+
         const containerDiv = document.querySelector('.container');
+        containerDiv.style.cssText = `grid-template-columns: repeat(${r}, ${800 / r}px);
+        grid-template-rows: repeat(${r}, ${800 / r}px);`;
         containerDiv.appendChild(newDiv);
     };
+    hoverColor();
 };
 
-for(let i = 0; i < quantity; i++){
-    const hoveredDiv = document.querySelectorAll('.square')[i];
-    hoveredDiv.addEventListener('mouseover', () => {
-        hoveredDiv.classList.add('hovered');
-    })
+function hoverColor() {
+    for (let i = 0; i < resolution ** 2; i++) {
+        const hoveredDiv = document.querySelectorAll('.square')[i];
+        hoveredDiv.addEventListener('mouseover', () => {
+            hoveredDiv.classList.add('hovered');
+        })
+    }
 }
